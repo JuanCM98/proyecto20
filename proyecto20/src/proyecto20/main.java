dbb7b358e1fd2450151887e6c9c88d2fc9dacff5
mint <mint@mint-VirtualBox>
2021-05-20 09:20:25 +0200

    Primera versión del proyecto solo contiene un método main.

---
+++ proyecto20/src/proyecto20/main.java
@@ -0,0 +1,15 @@
+package proyecto20;
+
+/**
+ * 
+ * @author Juan Cañadas Marqués
+ * @version 1.0.0
+ */
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
